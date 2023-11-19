47418dc3214b3dc39e38fc25ddb782ba18ecc243
Benjamin Phillip Hays <67854569+ultradestroyer9123@users.noreply.github.com>
2023-11-18 17:13:00 -0800

    Added the morse code function to blink_led.py

--- blink_led.py
+++ blink_led.py
@@ -2,8 +2,20 @@ import time
 from machine import Pin
 LED_PIN = Pin(0, Pin.OUT)
 
-while(True):
-    LED_PIN.value(1)
-    time.sleep(1)
-    LED_PIN.value(0)
-    time.sleep(1)
+morse_code_msg = ".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--".replace(" / ", "  ") # Hello world!
+
+for char in morse_code_msg:
+    print(char)
+    if char == '.':
+        LED_PIN.value(1)
+        time.sleep(0.05)
+        LED_PIN.value(0)
+        time.sleep(0.05)
+    elif char == '-':
+        LED_PIN.value(1)
+        time.sleep(0.15)
+        LED_PIN.value(0)
+        time.sleep(0.15)
+    elif char == " ":
+        time.sleep(0.3)
+    
